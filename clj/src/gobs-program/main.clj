f0139997402d684ac5a89e87c2206aec7b9b35e1
Dave Keller <keller@infinitemule.com>
2013-08-05 22:46:38 -0400

    Added Clojure command line version.

---
+++ clj/src/gobs-program/main.clj
@@ -0,0 +1,16 @@
+
+
+(defn gobs-program
+  []
+  (print "Gob's Program Y/N? ")
+  (flush) 
+  (let [answer (read-line)])
+  (while true (do (println "Penus Penus Penus Penus Penus Penus")))
+)
+
+
+(defn -main  
+  [& args]
+  (gobs-program))
+
+  
